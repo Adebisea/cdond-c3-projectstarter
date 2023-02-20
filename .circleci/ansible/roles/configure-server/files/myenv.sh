f8ad15cc635598a0ce719bbc348bf56f8d8cf604
Adebisea <abdulgkhadijah@gmail.com>
2023-02-20 12:00:33 +0100

    commented out exported variables

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++
@@ -1,9 +0,0 @@
-export TYPEORM_CONNECTION=postgres
-export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=udapeople-production.c7aujswgvxv5.us-east-1.rds.amazonaws.com
-export TYPEORM_PORT=5432
-export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD=udapeople
-export TYPEORM_DATABASE=postgres
-export TYPEORM_MIGRATION=./src/migrations/*.ts
-export TYPEORM_MIGRATIONS_DIR=./src/migrations
