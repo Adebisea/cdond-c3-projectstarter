cc975e8a2d70c072eb0363ce81938f17d3461c2b
Adebisea <abdulgkhadijah@gmail.com>
2023-03-09 10:19:44 +0100

    added the environment variables file

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,12 @@
+AWS_ACCESS_KEY_ID=AKIA4SIQIWS3MOZ2QO2L
+AWS_SECRET_ACCESS_KEY=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
+AWS_DEFAULT_REGION=us-east-2
+TYPEORM_CONNECTION=postgres
+TYPEORM_MIGRATIONS_DIR=./src/migrations
+TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+TYPEORM_MIGRATIONS=./src/migrations/*.ts
+TYPEORM_HOST=udapeople-production.czl4vkuuoxwd.us-east-2.rds.amazonaws.com
+TYPEORM_PORT=5432
+TYPEORM_USERNAME=postgres
+TYPEORM_PASSWORD=udapeople
+TYPEORM_DATABASE=postgres
